3d1175e2493498e130220c4684884ac5414e821a
kishibee <raflyade9@gmail.com>
2023-11-26 05:04:47 -0800

    homescreen

--- app/src/main/java/com/example/movielist/ui/screen/home/Home.kt
+++ app/src/main/java/com/example/movielist/ui/screen/home/Home.kt
@@ -1,13 +1,26 @@
 package com.example.movielist.ui.screen.home
 
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.animelist.R
 import com.example.movielist.ViewModelFactory
 import com.example.movielist.component.ButtonSearch
+import com.example.movielist.component.ItemMovie
+import com.example.movielist.component.ListEmpty
 import com.example.movielist.di.Injection
 import com.example.movielist.model.Movie
 import com.example.movielist.ui.common.UiState
@@ -48,7 +61,6 @@ fun Content(
     listMovie: List<Movie>,
     onFavoriteIcon: (newState: Boolean, id: Int) -> Unit,
     navigateToDetail: (Int) -> Unit,
-    modifier: Modifier = Modifier
 ) {
     Column {
         ButtonSearch(
@@ -62,7 +74,41 @@ fun Content(
                 onFavoriteIcon = onFavoriteIcon
             )
         } else {
-            empt
+            ListEmpty(
+                Warning = stringResource(R.string.empty_data))
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ListMovie(
+    listMovie: List<Movie>,
+    onFavoriteIcon: (newState: Boolean, id: Int) -> Unit,
+    navigateToDetail: (Int) -> Unit,
+    contentPaddingTop: Dp = 0.dp
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(
+            start = 16.dp,
+            bottom = 16.dp,
+            end = 16.dp,
+            top = contentPaddingTop
+        ),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(listMovie, key = { it.id }) { item ->
+            ItemMovie(
+                id = item.id,
+                name = item.name,
+                imageUrl = item.imageUrl,
+                rate = item.rate,
+                isFavorite = item.isFavorite,
+                onFavoriteIcon = onFavoriteIcon,
+                modifier = Modifier
+                    .animateItemPlacement(tween(durationMillis = 200))
+                    .clickable { navigateToDetail(item.id) }
+            )
         }
     }
 }
